8cade3ae7893c43e95a156b4fa24452b4e53a12b
Rodrigo Grzegorczyk <rodrigo.gregorio1@hotmail.com>
2022-06-22 21:34:27 -0300

    Componente Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Rodrigo Grzegorczyk"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, harum"
-      />
-      <Post
-        author="Rafael Gregorio"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, harum"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Rodrigo Grzegorczyk"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, harum"
+          />
+          <Post
+            author="Rafael Gregorio"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, harum"
+          />
+        </main>
+      </div>
     </div>
   )
 }
