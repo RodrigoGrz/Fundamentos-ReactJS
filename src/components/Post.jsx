673f757ff9c471c5e47843bb7fc6ba258989eb7c
Rodrigo Grzegorczyk <rodrigo.gregorio1@hotmail.com>
2022-06-25 13:48:07 -0300

    Component Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,13 @@
 import styles from './Post.module.css';
 import { Comment } from './Comment';
+import { Avatar } from './Avatar';
 
 export function Post() {
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/rodrigogrz.png" />
+                    <Avatar hasBorder src="https://github.com/rodrigogrz.png" />
                     <div className={styles.authorInfo}>
                         <strong>Rodrigo Grzegorczyk</strong>
                         <span>Web Developer</span>
