d077ba24d34b40bd5c4f464531a3daee1eecbc91
Rodrigo Grzegorczyk <rodrigo.gregorio1@hotmail.com>
2022-06-26 11:21:07 -0300

    Validando formulário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -31,7 +31,12 @@ export function Post({ author, publishedAt, content }) {
     }
 
     function handleNewCommentChange(event) {
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid(event) {
+        event.target.setCustomValidity("Esse campo é obrigatório");
     }
 
     function deleteComment(commentToDelete) {
@@ -41,6 +46,8 @@ export function Post({ author, publishedAt, content }) {
 
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
 
     return (
         <article className={styles.post}>
@@ -72,14 +79,18 @@ export function Post({ author, publishedAt, content }) {
                 <strong>Deixe seu feedback</strong>
 
                 <textarea
+                    required
                     placeholder="Deixe um comentário"
                     name="comment"
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
                     value={newCommentText}
                 />
 
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
 
